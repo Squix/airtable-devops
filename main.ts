f25824cd07374b2b3f8798e847fee4b65837dac6
Squix <squixdev.squix@gmail.com>
2025-02-25 11:55:26 +0100

    Add general CLI scaffolding and get-schema command squeleton.

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import { Command } from "@cliffy/command";
 
+// Define the version globally
+const VERSION = "1.0.0";
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   main();
@@ -7,10 +10,27 @@ if (import.meta.main) {
 
 //When CLI is run
 async function main() {
-  //main command
-  await new Command()
+  //Commands definition
+
+  //get-schema command
+  const get_schema_command = new Command()
+    .description(
+      "Generate schema files representing the structure of an Airtable base."
+    )
+    .action(async () => console.log("get-schema"));
+
+  const main_command = new Command()
+    //main command
     .name("airtable-devops")
-    .version("0.1.0")
-    .description("Devops tools for Airtable")
-    .parse(Deno.args);
+    .version(VERSION)
+    .description("DevOps tools for Airtable.")
+    .action(() => main_command.showHelp())
+    .command("get-schema", get_schema_command);
+
+  try {
+    await main_command.parse(Deno.args);
+  } catch (error: any) {
+    console.error("Error: ", error.message);
+    Deno.exit(1);
+  }
 }
